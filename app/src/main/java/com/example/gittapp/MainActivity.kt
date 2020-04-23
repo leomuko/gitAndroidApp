51e3f277792b91d7d3d1823844f7a46847284106
leomuko <kezzieleo@gmail.com>
2020-04-23 14:39:19 +0300

    Create intents on click to detail activity

--- app/src/main/java/com/example/gittapp/MainActivity.kt
+++ app/src/main/java/com/example/gittapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.gittapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), onItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +22,17 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        recycler_view.adapter = RecyclerAdapter(theList)
+        recycler_view.adapter = RecyclerAdapter(theList, this)
         recycler_view.layoutManager = GridLayoutManager(this,2)
         recycler_view.setHasFixedSize(true)
 
+
+    }
+
+    override fun onItemClick(item: RecyclerItem, position: Int) {
+        //Toast.makeText(this, "I have been clicked", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
     }
 
 }
--- app/src/main/java/com/example/gittapp/RecyclerAdapter.kt
+++ app/src/main/java/com/example/gittapp/RecyclerAdapter.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recycler_screen.view.*
 
-class RecyclerAdapter(private val recyclerList: List<RecyclerItem>): RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
+class RecyclerAdapter(private val recyclerList: List<RecyclerItem>, var clickListener: onItemClickListener): RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -22,16 +22,30 @@ class RecyclerAdapter(private val recyclerList: List<RecyclerItem>): RecyclerVie
 
     override fun onBindViewHolder(holder: RecyclerAdapter.RecyclerViewHolder, position: Int) {
         val currentItem = recyclerList[position]
-        holder.userImage1.setImageResource(currentItem.imageResource1)
+       /* holder.userImage1.setImageResource(currentItem.imageResource1)
         holder.followers.text = currentItem.text1
-        holder.userName.text = currentItem.text2
+        holder.userName.text = currentItem.text2*/
+        holder.initialize(recyclerList.get(position), clickListener)
     }
 
     class RecyclerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val userImage1: ImageView = itemView.user_image
         val followers: TextView = itemView.follower
         val userName : TextView = itemView.username
+
+        fun initialize(item : RecyclerItem, action : onItemClickListener){
+            userImage1.setImageResource(item.imageResource1)
+            followers.text = item.text1
+            userName.text = item.text2
+
+            itemView.setOnClickListener{
+                action.onItemClick(item,adapterPosition)
+            }
+        }
+
     }
-
-
+   
+}
+interface onItemClickListener{
+    fun onItemClick(item : RecyclerItem, position: Int)
 }
