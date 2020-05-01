00c49cac99d5f3c8f3cfc190878c0de93a6c301c
leomuko <kezzieleo@gmail.com>
2020-05-01 16:34:14 +0300

    Create UserModel

---
+++ app/src/main/java/com/example/gittapp/models/UserModel.kt
@@ -0,0 +1,12 @@
+package com.example.gittapp.models
+
+import com.google.gson.annotations.SerializedName
+
+data class UserModel(
+    @SerializedName("login")
+    val userName : String,
+    @SerializedName("followers")
+    var followers: Int,
+    @SerializedName("following")
+    var following: Int
+)
